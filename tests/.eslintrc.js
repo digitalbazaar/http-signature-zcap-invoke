630c9f570179d400ac5e11f05c2f728a887ce45a
Andrew Jones <ajones@digitalbazaar.com>
2020-01-29 17:01:35 -0500

    Add initial sign test.

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
+  globals: {
+    should: true
+  },
   env: {mocha: true}
 }
--- tests/10-sign.spec.js
+++ tests/10-sign.spec.js
@@ -1,18 +1,29 @@
 const {signCapabilityInvocation} = require('../index');
+const {Ed25519KeyPair} = require('crypto-ld');
+const {authorizedRequest} = require('./test-assertions');
 
 describe('signCapabilityInvocation', function() {
+  let ed25519Key, keyId = null;
+  before(async function() {
+    ed25519Key = await Ed25519KeyPair.generate();
+    keyId = 'did:test:foo';
+    ed25519Key.id = keyId;
+  });
   it('should sign basic request', async function() {
+    const invocationSigner = ed25519Key.signer();
+    invocationSigner.id = keyId;
     const signed = await signCapabilityInvocation({
       url: 'https://www.test.org/read/foo',
       method: 'GET',
-      headers: {date: new Date().toUTCString()},
-      json: {foo: true},
-      invocationSigner: {
-        id: 'did:test:foo',
-        sign() {console.log('signed');}
+      headers: {
+        keyId,
+        date: new Date().toUTCString()
       },
+      json: {foo: true},
+      invocationSigner,
       capabilityAction: 'read'
     });
     console.log('signed', signed);
+    authorizedRequest(signed);
   });
 });
