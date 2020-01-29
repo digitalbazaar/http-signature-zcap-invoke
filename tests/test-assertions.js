5150cd9d9426fa3bcca70650d95bc0f71da05c4e
Andrew Jones <ajones@digitalbazaar.com>
2020-01-29 17:01:35 -0500

    Add initial sign test.

---
+++ tests/test-assertions.js
@@ -0,0 +1,19 @@
+const authorizedRequest = actualResult => {
+  should.exist(actualResult);
+  actualResult.should.be.an('object');
+  actualResult.keyid.should.exist;
+  actualResult.keyid.should.be.a('string');
+  actualResult.date.should.exist;
+  actualResult.date.should.be.a('string');
+  actualResult.host.should.exist;
+  actualResult.host.should.be.a('string');
+  actualResult.digest.should.exist;
+  actualResult.digest.should.be.a('string');
+  actualResult.authorization.should.exist;
+  actualResult.authorization.should.be.a('string');
+  actualResult.authorization.should.contain('keyId');
+  actualResult.authorization.should.contain('headers');
+  actualResult.authorization.should.contain('signature');
+};
+
+exports.authorizedRequest = authorizedRequest;
